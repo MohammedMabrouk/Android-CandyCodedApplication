c3069fbec77c7499522781d9e204500f9e00eacd
Mohamed <mohamedmabrouk180@gmail.com>
2019-10-26 18:28:57 +0200

    task 4

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -92,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
 
